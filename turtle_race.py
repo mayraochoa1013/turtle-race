99301540e3db304d5f182de6adfaae4e5376e972
Mayra Ochoa <mayra@Admins-MacBook-Pro.local>
2021-04-06 12:57:02 -0700

    Initial Code

---
+++ turtle_race.py
@@ -0,0 +1,22 @@
+#import libraries & modules
+
+#create a window
+
+speed(0)
+penup()
+goto(-140, 140)
+
+#define functions
+
+
+# create a for loop that helps us draw the dashed track lines
+
+# Here we create a turtle
+
+
+# Let's send the turtle to the starting line
+
+
+# This for loop makes the turtle twirl
+
+# Create a for loop that makes our turtles race. Add your turtles to the race.
